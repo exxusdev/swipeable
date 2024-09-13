4b6dac00320759d30028be1668af4f5cc9cf0f34
Jonas <jonascorreia@plenustech.com.br>
2024-09-13 18:07:17 -0400

    last commit

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,5 +1,6 @@
+import { useRef } from "react";
 import { View, FlatList } from "react-native";
-import Swipeable from "react-native-gesture-handler/ReanimatedSwipeable";
+import Swipeable, { SwipeableMethods } from "react-native-gesture-handler/ReanimatedSwipeable";
 
 import { stylesHome } from "./styles";
 import { contacts } from "../../utils/contacts";
@@ -8,11 +9,23 @@ import { Card } from "../../components/card";
 import { Option } from "../../components/option";
 
 export function Home(){
+    const openSwipeableRef = useRef<SwipeableMethods | null>(null);
 
-    function OnSwipeableWillOpen(direction: "left" | "right") {
-        (direction === "left") 
-            ? console.warn("Deletar") 
-            : console.warn("Arquivar")
+    function onSwipeableWillOpen(
+        direction: "left" | "right", 
+        currentItem: SwipeableMethods | null
+    ) {
+        if (direction === "left") {
+            console.warn("DELETAR!") 
+        }            
+
+        if (openSwipeableRef.current) {
+            !(openSwipeableRef.current === currentItem) 
+                ? openSwipeableRef.current.close()            
+                : undefined
+        }
+
+        openSwipeableRef.current = currentItem
     }
 
     return (
@@ -20,28 +33,32 @@ export function Home(){
             <FlatList 
                 data={contacts}
                 keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                    <Swipeable
-                        containerStyle={stylesHome.swipeableContainer}
-                        overshootRight={false}
-                        onSwipeableWillOpen={OnSwipeableWillOpen}                            
-                        
-                        renderRightActions={() => (
-                            <View style={stylesHome.rightActions}>
-                                <Option icon="archive" backgroundColor="#00B960"/>
-                                <Option icon="open-in-new" backgroundColor="#3E68D7"/>
-                            </View>
-                        )}
-                        
-                        renderLeftActions={() => (
-                            <View style={stylesHome.leftActions}>                              
-                                <Option icon="delete" backgroundColor="#E83D55"/>
-                            </View>
-                        )}
-                    >
-                        <Card name={item.name} email={item.email} />
-                    </Swipeable>
-                )}
+                renderItem={({ item }) => {
+                    let currentItem: SwipeableMethods | null = null
+
+                    return (
+                        <Swipeable
+                            ref={(swipeable) => (currentItem = swipeable)}
+                            containerStyle={stylesHome.swipeableContainer}
+                            overshootRight={false}
+                            onSwipeableWillOpen={(direction) => onSwipeableWillOpen(direction, currentItem)}
+                            
+                            renderRightActions={() => (
+                                <View style={stylesHome.rightActions}>
+                                    <Option icon="archive" backgroundColor="#00B960"/>
+                                    <Option icon="open-in-new" backgroundColor="#3E68D7"/>
+                                </View>
+                            )}
+                            
+                            renderLeftActions={() => (
+                                <View style={stylesHome.leftActions}>                              
+                                    <Option icon="delete" backgroundColor="#E83D55"/>
+                                </View>
+                            )}
+                        >
+                            <Card name={item.name} email={item.email} />
+                        </Swipeable>)
+                    }}
                 contentContainerStyle={stylesHome.content}
                 showsVerticalScrollIndicator={false}
             />               
